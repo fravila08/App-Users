c0aaeae4cf9db6e891d6890d7784687a1b9f2145
fravila08 <105952966+fravila08@users.noreply.github.com>
2022-05-31 11:12:44 -0700

    Done

--- User.py
+++ User.py
@@ -1,1 +1,16 @@
-# your User class goes here
+class Person:
+    def __init__(self,name,email_address,liscence_number):
+        self.name=name
+        self.email_address=email_address
+        self.liscence_number=liscence_number
+    def __str__(self):
+        return f"Hi, my name is {self.name}, my email and liscence number are {self.email_address}, {self.liscence_number}."
+    #end of class
+    
+esmeralda=Person('Esmeralda', 'esm@gmail.org','7TFG321')
+francisco=Person('Esmeralda', 'fran@gmail.org','123qwe567')
+dennis=Person('Dennis', 'den@gmail.org','1qgsd5767')
+
+print(esmeralda)
+print(francisco)
+print(dennis)
